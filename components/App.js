79801ba38e1876c3da910d6bf1960c6155028699
RafBro8 <rromn8@gmail.com>
2018-03-20 21:44:18 -0500

    added categories and category components, added actions and reducers, added routing, created Store

--- components/App.js
+++ components/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setCategories, pickCategory } from "../actions/index";
+
+
 
 
 class App extends Component {
 
     componentDidMount() {
-        fetch('http://jservice.io/api/categories?count=20')
-            .then(response => response.json())
-            .then(json => console.log(json));
+        if(this.props.categories.length === 0) {
+            fetch('http://jservice.io/api/categories?count=20')
+                .then(response => response.json())
+                .then(json => this.props.setCategories(json));
+       //make sure fetch happens only once
+        }
     }
 
     render() {
-        return(
+        console.log('App props', this.props);
+
+        return (
             <div>
                 <h2>Jeopardy!</h2>
+                {
+                    this.props.categories.map(category => {
+                        return(
+                        <div key={category.id}>
+                            <Link
+                                to='/category'
+                                onClick={() => this.props.pickCategory(category)}
+                            >
+                                <h4>{category.title}</h4>
+                            </Link>
+                        </div>
+                        )
+                    })
+                }
             </div>
         )
     }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return { categories: state.categories }
+
+}
+
+export default connect(mapStateToProps, { setCategories, pickCategory })(App);
+
+
+//mapStateToProps() describes what part of the Redux Store we want
+//to make available as Props on this Component
+
+//{setCategories} Action Creator passed to the Props of this Component
+//console.log('App props', this.props);
+
---
+++ components/Category.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+
+class Category extends Component {
+    render() {
+        console.log('this.props.category', this.props);
+
+        return (
+            <div>
+                <Link className='link-home' to='/'><h4>Home</h4></Link>
+                <h2>Category title</h2>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return { category: state.category}
+
+}
+
+export default connect(mapStateToProps, null)(Category);
